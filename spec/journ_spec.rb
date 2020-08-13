641c5e93829adbf3eb4119361a92f831f5bdf777
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-13 16:14:28 +0100

    Added a failing test to calc the fare charge

--- spec/journ_spec.rb
+++ spec/journ_spec.rb
@@ -1,6 +1,9 @@
 require "journey"
 
 describe Journey do
+  let(:station_1) { double :station_1, zone: 1 }
+  let(:station_2) { double :station_2, zone: 2 }
+  let(:station_3) { double :station_2, zone: 3 } 
 
   it 'is initially not in a journey' do
     expect(subject).not_to be_in_journey
@@ -36,6 +39,12 @@ describe Journey do
     it "Charges the penalty fare it you don't end the trip" do
       subject.start_trip("Old Street")
       expect(subject.fare).to eq Journey::PENALTY_FARE
+    end
+
+    it 'charges the correct fare for zones 1 to 2' do
+      subject.start_trip(station_1)
+      subject.end_trip(station_2)
+      expect(subject.fare).to eq 2
     end
 
   end
